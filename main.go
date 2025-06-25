f8402c6a2500bba38b2a7f6940d1e65c92a1adc9
Moaz Ahmed <justmeyo2025@gmail.com>
2025-06-25 19:13:07 +0300

    add: User Scanning Input feature.

--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	cleanInput("  hello  world  ")
+	// wait for user command
+	scanner := bufio.NewScanner(os.Stdin)
+	var userInput string
+
+	// intro
+	fmt.Print("Pokedex >")
+
+	// loop through each userInput after entering "return key"
+	for scanner.Scan() {
+
+		// get user input
+		userInput = scanner.Text()
+
+		// get each word separated
+		textSli := cleanInput(userInput)
+
+		// clear userInput
+		userInput = ""
+
+		if len(textSli) != 0 {
+			// print just the first word
+			fmt.Printf("Your command was: %s\n", strings.ToLower(textSli[0]))
+		} else {
+			fmt.Println("Invalid Input")
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Invalid Input: %w", err)
+	}
+
 }
 
 // cleanInput splits the input text into words, handling all whitespace characters
